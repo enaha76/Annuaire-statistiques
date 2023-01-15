e06be97e1f25015928714edf195bb906ea2588e0
Ahmedou <21076@supnum.mr>
2023-01-15 22:54:55 +0100

    export 2 mt7at

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -294,14 +294,14 @@
                                         </div>
                                     </li>
                                     <li>
-<<<<<<< HEAD
+{{-- <<<<<<< HEAD --}}
                                         
-                                        <a href="#">Export</a>
+                                        {{-- <a href="#">Export</a> --}}
 
-=======
+{{-- ======= --}}
                                         {{-- {{ route('layout-sidenav-light') }} --}}
                                         <a href="{{ route('Export') }}">Export</a>
->>>>>>> 6907f391dd426189701d726f479410748ed76137
+{{-- >>>>>>> 6907f391dd426189701d726f479410748ed76137 --}}
                                     </li>
                                 </ul>
                             </div>
