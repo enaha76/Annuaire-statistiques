86c8f014d777c3289d1ea0d49beea3f7fe1b1cbb
Ahmedou <21076@supnum.mr>
2023-01-18 13:22:22 +0100

    change cofirm place

--- resources/views/confirm_import.blade.php
+++ resources/views/confirm_import.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
    <style>
-    .overlay {
+    /* .overlay {
   position: fixed;
   top: 0;
   bottom: 0;
@@ -25,34 +25,67 @@
   background-color: white;
   border-radius: 5px;
   padding: 20px;
-}
+} */
 
     </style>         
-<div class="overlay" id="myOverlay">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title" id="exampleModalLabel">Attention</h1>
-          <button type="button" class="close" onclick="closeOverlay()">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <form action="{{route('insert')}}" method="POST">
-                @csrf
-<h3>les inscripition pour cette annee dans cette etablissement sont deja importee voulez vous les ecrase?</h3>
-<input type="hidden" name="establishment" value="{{$request['establishment']}}">
-<input type="hidden" name="year" value="{{$request['year']}}">
-<textarea name="file" style="display: none" >{{$request['file']}}</textarea>
-<a href="{{route('tables')}}" class="btn btn-danger">Annuler</a>
-<input class="btn btn-success" type="submit" value="continue">
 
-            </form>
-        </div>
+         
+          
+            
+           {{-- <div class="modal-body"> --}}
+             {{-- <form action="{{route('insert')}}" method="POST">
+                @csrf
+              <h3>les inscripition pour cette annee dans cette etablissement sont deja importee voulez vous les ecrase ?</h3>
+              <input type="hidden" name="establishment" value="{{$request['establishment']}}">
+              <input type="hidden" name="year" value="{{$request['year']}}">
+              <textarea name="file" style="display: none" >{{$request['file']}}</textarea>
+              <a href="{{route('tables')}}" class="btn btn-danger">Annuler</a>
+           <input class="btn btn-success" type="submit" value="continue">
+
+            </form> --}}
+        {{-- </div> --}}
+        <div class="row">
+          <div class="col-12">
+              <div class="page-title-box">
+                  <div class="page-title-right">
+                      <ol class="breadcrumb m-0">
+                          {{-- <li class="breadcrumb-item"><a href="javascript: void(0);"></a></li> --}}
+                          {{-- <li class="breadcrumb-item"><a href="javascript: void(0);"></a></li> --}}
+                          <li class="breadcrumb-item active"></li>
+                      </ol>
+                  </div>
+                  <h4 class="page-title"></h4>
+              </div>
+          </div>
       </div>
-    </div>
-  </div>
-<script>
+      <br> <br>  
+                                <div class="card text-white bg-dark">
+                                    <div class="card-body">
+                                        <blockquote class="card-bodyquote">
+                                          <h1 class="modal-title" id="exampleModalLabel">Attention</h1>
+                                          {{-- <button type="button" class="close" onclick="closeOverlay()">
+
+                                          <span aria-hidden="true">&times;</span> --}}
+                                          {{-- </button> --}}
+                                          <br> <br>  <br> 
+                                          <form action="{{route('insert')}}" method="POST">
+                                            @csrf
+                                            <h4>les inscripition pour cette annee dans cette etablissement sont deja importee voulez vous les ecrase ?</h4>
+                                            <input type="hidden" name="establishment" value="{{$request['establishment']}}">
+                                            <input type="hidden" name="year" value="{{$request['year']}}">
+                                            <textarea name="file" style="display: none" >{{$request['file']}}</textarea>
+                                            <a href="{{route('tables')}}" class="btn btn-danger">Annuler</a>
+                                            <input class="btn btn-primary" type="submit" value="continue">
+                                            {{-- <footer>Someone famous in <cite title="Source Title">Source Title</cite>
+                                            </footer> --}}
+                                          </form>
+                                        </blockquote>
+                                    </div> <!-- end card-body-->
+                                </div> <!-- end card-->
+                            </div> <!-- end col-->
+                        
+      
+{{-- <script>
     function closeOverlay() {
     location.href = "javascript:history.back()";
 }
@@ -60,5 +93,5 @@ function myFunction(var1, var2,var3) {
     fetch( + var1 + '/' + var2+'/'+var3)
       
 }
-</script>  
+</script>   --}}
 @endsection
