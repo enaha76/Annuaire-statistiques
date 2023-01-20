165e7a4769be4981146c525c91bba99a4449053b
Ahmedou <21076@supnum.mr>
2023-01-20 19:14:21 +0100

    create aray for etablisment and theire followig up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -135,148 +135,7 @@
                                     </div>
                                 </li>
 
-                                {{-- <li class="dropdown notification-list">
-                                    <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                        <i class="dripicons-bell noti-icon"></i>
-                                        <span class="noti-icon-badge"></span>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg">
 
-                                        <!-- item-->
-                                        <div class="dropdown-item noti-title">
-                                            <h5 class="m-0">
-                                                <span class="float-end">
-                                                    <a href="javascript: void(0);" class="text-dark">
-                                                        <small>Clear All</small>
-                                                    </a>
-                                                </span>Notification
-                                            </h5>
-                                        </div>
-
-                                        <div style="max-height: 230px;" data-simplebar="">
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon bg-primary">
-                                                    <i class="mdi mdi-comment-account-outline"></i>
-                                                </div>
-                                                <p class="notify-details">Caleb Flakelar commented on Admin
-                                                    <small class="text-muted">1 min ago</small>
-                                                </p>
-                                            </a>
-
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon bg-info">
-                                                    <i class="mdi mdi-account-plus"></i>
-                                                </div>
-                                                <p class="notify-details">New user registered.
-                                                    <small class="text-muted">5 hours ago</small>
-                                                </p>
-                                            </a>
-
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon">
-                                                    <img src="assets/images/users/avatar-2.jpg" class="img-fluid rounded-circle" alt=""> </div>
-                                                <p class="notify-details">Cristina Pride</p>
-                                                <p class="text-muted mb-0 user-msg">
-                                                    <small>Hi, How are you? What about our next meeting</small>
-                                                </p>
-                                            </a>
-
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon bg-primary">
-                                                    <i class="mdi mdi-comment-account-outline"></i>
-                                                </div>
-                                                <p class="notify-details">Caleb Flakelar commented on Admin
-                                                    <small class="text-muted">4 days ago</small>
-                                                </p>
-                                            </a>
-
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon">
-                                                    <img src="assets/images/users/avatar-4.jpg" class="img-fluid rounded-circle" alt=""> </div>
-                                                <p class="notify-details">Karen Robinson</p>
-                                                <p class="text-muted mb-0 user-msg">
-                                                    <small>Wow ! this admin looks good and awesome design</small>
-                                                </p>
-                                            </a>
-
-                                            <!-- item-->
-                                            <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                                <div class="notify-icon bg-info">
-                                                    <i class="mdi mdi-heart"></i>
-                                                </div>
-                                                <p class="notify-details">Carlos Crouch liked
-                                                    <b>Admin</b>
-                                                    <small class="text-muted">13 days ago</small>
-                                                </p>
-                                            </a>
-                                        </div>
-
-                                        <!-- All-->
-                                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
-                                            View All
-                                        </a>
-
-                                    </div>
-                                </li> --}}
-
-                                {{-- <li class="dropdown notification-list d-none d-sm-inline-block">
-                                    <a class="nav-link dropdown-toggle arrow-none" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                                        <i class="dripicons-view-apps noti-icon"></i>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated dropdown-lg p-0">
-
-                                        <div class="p-2">
-                                            <div class="row g-0">
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/slack.png" alt="slack">
-                                                        <span>Slack</span>
-                                                    </a>
-                                                </div>
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/github.png" alt="Github">
-                                                        <span>GitHub</span>
-                                                    </a>
-                                                </div>
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/dribbble.png" alt="dribbble">
-                                                        <span>Dribbble</span>
-                                                    </a>
-                                                </div>
-                                            </div>
-
-                                            <div class="row g-0">
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/bitbucket.png" alt="bitbucket">
-                                                        <span>Bitbucket</span>
-                                                    </a>
-                                                </div>
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/dropbox.png" alt="dropbox">
-                                                        <span>Dropbox</span>
-                                                    </a>
-                                                </div>
-                                                <div class="col">
-                                                    <a class="dropdown-icon-item" href="#">
-                                                        <img src="assets/images/brands/g-suite.png" alt="G Suite">
-                                                        <span>G Suite</span>
-                                                    </a>
-                                                </div>
-                                            </div> <!-- end row-->
-                                        </div>
-
-                                    </div>
-                                </li>
-     --}}
                                 <li class="notification-list">
                                     <a class="nav-link end-bar-toggle" href="javascript: void(0);">
                                         <i class="dripicons-gear noti-icon"></i>
@@ -508,7 +367,23 @@
                                            
                                             <div  data-simplebar="" style="max-height: 300px;">
                                             <h4 class="header-title mb-3">Effectifs des étudiants par établissement du public</h4>
+                                                 @php
 
+                                                 $array_eteblissment=[];                       
+                                                       foreach ($List2 as $one) {
+                                                        $v=$one->abrev;
+                                                        $m=[];
+                                                        foreach (collect($List3)->where('identifiant', $one->id) as $value) {                                                           
+                                                            $m[]=$value->abrev;                                                           
+                                                        };
+                                                        if(!$m){
+                                                            if ($one->type=='institut') {                                         
+                                                            }
+                                                        $m[]=$v;
+                                                        }
+                                                         $array_eteblissment[$v]=$m;  
+                                                    }                                                  
+                                                 @endphp
                                          
                                                                 <h4 class="header-title mb-3"></h4>
                                                                 <div class="table-responsive">
@@ -521,7 +396,7 @@
                                                                         </tr>
                                                                         </thead>
                                                                         <tbody id="tab">
-                                                                            @foreach ( $List as $list=>$one )
+                                                                            @foreach ( $List2 as $list=>$one )
                                                                                <tr>
                                                                                 <td >
                                                                                     {{ $one->abrev }}
@@ -634,7 +509,15 @@
                                                  </div> --}}
                                              </div>
 
-                                            <h5 class="mb-1 mt-0 fw-normal"></h5>
+                                            <h5 class="mb-1 mt-0 fw-normal">
+                                                {{-- @php
+                                                    foreach ($etablissements) {
+                                                             
+                                                      echo $key;
+                                                    }
+                                                    echo i
+                                                @endphp --}}
+                                            </h5>
                                             <div class="progress-w-percent">
                                                 <span class="progress-value fw-bold"></span>
                                                 {{-- <div class="progress progress-sm">
