3ab621495b7054c43a28498d59ae132651e96631
djilit <ahmedoudjilit@gmail.com>
2023-01-21 19:28:55 +0100

    conflit fixed

--- resources/views/tables.blade.php
+++ resources/views/tables.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+{{-- @php
+    print_r($List4);
+@endphp --}}
   <!-- start page title -->
+  <div class="text-center" >
+    <div class="spinner-border avatar-lg text-primary" role="status" style="position: absolute; top: 50%; left: 50%; margin: -1.5rem 0 0 -1.5rem;  display: none;"aria-labelledby="danger-header-modalLabel" aria-hidden="true"></div>
+</div>
   <div class="container-fluid">
   @if (session('success'))
-  <div class="alert alert-success" role="alert">
-    <div style="display: flex; align-items: center;">
-      <i class="fas fa-check-circle"></i>
-      <span class="ml-2">{{ session('success') }}</span>
-    </div>
+ 
+  <div >
+    <div  id="success-alert-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content modal-filled bg-success">
+                <div class="modal-body p-4">
+                    <div class="text-center">
+                        <i class="dripicons-checkmark h1"></i>
+                        <h4 class="mt-2">Well Done!</h4>
+                        <p class="mt-3">Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                        <button type="button" class="btn btn-light my-2" data-bs-dismiss="modal">Continue</button>
+                    </div>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    
   </div>
   @endif
   <div class="row">
@@ -156,40 +174,95 @@ data-upload-preview-template="#uploadPreviewTemplate"> --}}
 </div>
 
 
-                                                <!-- Danger Header Modal -->
-                                                <div id="danger-header-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="danger-header-modalLabel" aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header modal-colored-header bg-danger">
-                                                                <h4 class="modal-title" id="danger-header-modalLabel">Modal Heading</h4>
-                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <h5 class="mt-0">error of eteblisment</h5>
-                                                                {{-- <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                                                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p> --}}
-                                                                <p> you haven't chosen any eteblissment so please use one </p>
-                                                            </div>
-                                                            {{-- <div class="modal-footer">
-                                                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                                                                <button type="button" class="btn btn-danger">Save changes</button>
-                                                            </div> --}}
-                                                        </div><!-- /.modal-content -->
-                                                    </div><!-- /.modal-dialog -->
-                                                </div><!-- /.modal -->
-        
-                                                <div class="button-list">
-                                                   
-                                                    {{-- <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#danger-header-modal">Danger Header</button> --}}
-                                                   
-                                                </div>
-   
-</section>
+<!-- Danger Header Modal -->
+<div id="danger-header-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="danger-header-modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header modal-colored-header bg-danger">
+                <h4 class="modal-title" id="danger-header-modalLabel">Modal Heading</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h5 class="mt-0">error of eteblisment</h5>
+                {{-- <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p> --}}
+                <p> you haven't chosen any eteblissment so please use one </p>
+            </div>
+            {{-- <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger">Save changes</button>
+            </div> --}}
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div id="warning-alert-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-body p-4">
+                <div class="text-center">
+                    <i class="dripicons-warning h1 text-warning"></i>
+                    <h4 class="mt-2">Incorrect Information</h4>
+                    <p class="mt-3">Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <button type="button"  id="kk" class="btn btn-warning my-2" data-bs-dismiss="modal">Continue</button>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<div id="danger-alert-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content modal-filled bg-danger">
+            <div class="modal-body p-4">
+                <div class="text-center">
+                    <i class="dripicons-wrong h1"></i>
+                    <h4 class="mt-2">Oh snap!</h4>
+                    <p class="mt-3">Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <button type="button" id="jj" class="btn btn-light my-2" data-bs-dismiss="modal">Continue</button>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div id="info-header-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="info-header-modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header modal-colored-header bg-info">
+                <h4 class="modal-title" id="info-header-modalLabel">Modal Heading</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h5 class="mt-0">Info Background</h5>
+                <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            </div>
+            {{-- <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-info">Save changes</button>
+            </div> --}}
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->                                             
+                                             
+</section>                     
   </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.1/xlsx.core.min.js"></script>
 <!-- end demo js-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+        <script>
+            // $(window).on('load', function(){
+            //     $('.spinner-border').fadeIn();
+            //   });
+              
+            //   $(document).ready(function(){
+            //     $('.spinner-border').fadeOut(2000);
+            //   });
+           
+        </script>
 <script >
     $('#search').keyup(function(){
       var value= $(this).val().toLowerCase();
@@ -208,6 +281,18 @@ data-upload-preview-template="#uploadPreviewTemplate"> --}}
 <!-- init js -->
 <script src="assets/js/ui/component.fileupload.js"></script>
 <script>
+
+</script>
+<script>
+    jsonData = @json($data);
+    window.jsonData=jsonData;
+    // console.log(jsonData);
+    $(document).ready(function(){
+    setTimeout(function(){
+        $("#success-alert-modal").modal("show");
+    }, 100);
+});
+
 
 </script>
 <script src="assets/js/pages/rens.js">
@@ -234,4 +319,5 @@ function customInput (el) {
   }
 }
 </script>
+
        @endsection
