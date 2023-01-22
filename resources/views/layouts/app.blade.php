6d9602f88555d2e6c073ff8a60c5b3dc824a9f59
djilit <ahmedoudjilit@gmail.com>
2023-01-22 23:04:02 +0100

    page student modidied

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,122 +1,3 @@
-{{-- 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
-        <link href="{{ asset('cdn/PI_workspace.css')}}" rel="stylesheet" />
-        <link href="{{ asset('css/styles.css')}}" rel="stylesheet" />
-        <link href="{{ asset('bootstrap.min.css')}}" rel="stylesheet" />
-        <script src="{{ asset('cdn/all.js')}}" crossorigin="anonymous"></script>
-        {{-- <script src="{{ asset('bootstrap.min.js')}}"></script>
-        <script src="{{ asset('jquery-3.2.1.slim.min.js')}}"></script>
-        <script src="{{ asset('popper.min.js')}}"></script> --}}
-        
-    {{-- </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3 font-weight-bold" href="{{route('index')}}">DPSE</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-                </div>
-            </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">catagorie</div>
-                            <a class="nav-link" href="{{ route('etudiants') }}">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Etudiants
-                            </a>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Export
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="{{ route('layout-sidenav-light') }}">etudiant</a>
-                                </nav>
-                            </div>
-                           
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Import
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link" href="{{ route('tables') }}">etudiant</a>
-                                    
-                                </nav>
-                            </div>
-                         
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                      
-                        DPSE
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-           
-@yield('content')
-
-
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2022</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        
-        <script src="{{ asset('cdn/bootstrap.bundle.min.js')}}" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/scripts.js')}}"></script>
-        <script src="{{ asset('cdn/Chart.min.js')}}" crossorigin="anonymous"></script>
-         <script src="{{asset('assets/demo/chart-area-demo.js')}}"></script> 
-         <script src="{{asset('assets/demo/chart-bar-demo.js')}}"></script> 
-         <script src="{{asset('assets/demo/chart-pie-demo.js')}}"></script> 
-        <script src="{{ asset('cdn/simple-datatables@latest.js')}}" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/datatables-simple-demo.js')}}"></script>
-    
-    
-    
-    </body> --}}
-{{-- </html>  --}}
-
 <!DOCTYPE html>
     <html lang="en">
 
@@ -493,7 +374,7 @@
                                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated topbar-dropdown-menu profile-dropdown">
                                     <!-- item-->
                                     <div class=" dropdown-header noti-title">
-                                        <h6 class="text-overflow m-0">Welcome !</h6>
+                                        <h6 class="text-overflow m-0">Bienvenue !</h6>
                                     </div>
 
                                     <!-- item-->
@@ -610,7 +491,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6">
-                                <script>document.write(new Date().getFullYear())</script> © SupNum - Coderthemes.com
+                                <script>document.write(new Date().getFullYear())</script> © SupNum - supnumCoder.com
                             </div>
                             <div class="col-md-6">
                                 <div class="text-md-end footer-links d-none d-md-block">
